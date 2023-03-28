2ffc2b45acec084c2936c199dbd6e256d6e7ba34
dipanjan115 <dipanjan.adhikary@nordicsemi.no>
2023-03-28 15:06:43 +0200

    working

--- subsys/bluetooth/mesh/net.c
+++ subsys/bluetooth/mesh/net.c
@@ -792,7 +792,7 @@ int bt_mesh_net_decode(struct net_buf_simple *in, enum bt_mesh_net_if net_if,
 
 	LOG_DBG("src 0x%04x dst 0x%04x ttl %u", rx->ctx.addr, rx->ctx.recv_dst, rx->ctx.recv_ttl);
 	LOG_DBG("PDU: %s", bt_hex(out->data, out->len));
-	LOG_DBG("from src 0x%04x to dst 0x%04x\n", rx->ctx.addr, rx->ctx.recv_dst);
+	LOG_DBG("seq 0x%06x from src 0x%04x to dst 0x%04x\n",rx->seq, rx->ctx.addr, rx->ctx.recv_dst);
 	msg_cache_add(rx);
 
 	return 0;
